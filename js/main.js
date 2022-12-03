94ebc1669ff148feb157c8f953c310b1d2ea1b25
thiagosaidm <thiag.said@gmail.com>
2022-12-03 20:30:06 -0300

    "#"

--- js/main.js
+++ js/main.js
@@ -1,9 +1,6 @@
 
 /* ScrollReveal */
 
-var smarthPhone = window.matchMedia("(max-width: 600px)")
-
-if (smarthPhone <= window.matchMedia("(max-width:800px")) {
     window.sr = ScrollReveal({ reset: true });
 
     sr.reveal('.sub', { delay: 1000 });
@@ -12,11 +9,6 @@ if (smarthPhone <= window.matchMedia("(max-width:800px")) {
     sr.reveal('.habilidades', { delay: 1000 });
     sr.reveal('.sobre', { delay: 1000 });
     sr.reveal('.contato', { delay: 500 });
-} else {
-
-    window.sr = ScrollReveal({ reset: false });
-
-}
 
 
 
