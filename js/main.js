7363e16b7037cc85754ef29a48b30ae95c1cd59c
thiagosaidm <thiag.said@gmail.com>
2022-12-04 11:06:15 -0300

    "#"

--- js/main.js
+++ js/main.js
@@ -10,6 +10,17 @@
     sr.reveal('.sobre', { delay: 1000 });
     sr.reveal('.contato', { delay: 500 });
 
+/* Menu Hamburger */
+
+const menuBtn = document.querySelector(".btn_menu");
+
+function mostrarMenu() {
+    const nav = document.getElementById('nav')
+    nav.classList.toggle('on');
+}
+
+menuBtn.addEventListener("click", mostrarMenu)
+
 
 
 
